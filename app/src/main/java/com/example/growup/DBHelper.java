0fd895f69dee1c7bed4d96163fbabbc858ec1394
khalilTeam <khoojagi.khalil@gmail.com>
2024-07-19 02:42:04 +0330

    let's wait for push all project files for mvp

---
+++ app/src/main/java/com/example/growup/DBHelper.java
@@ -0,0 +1,224 @@
+package com.example.growup;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteConstraintException;
+import android.widget.Toast;
+import net.sqlcipher.database.SQLiteDatabase;
+import net.sqlcipher.database.SQLiteOpenHelper;
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "GrowUpDataBase";
+    public static final int DATABASE_VERSION = 12;
+    public static SQLiteDatabase dbReadable;
+    public static SQLiteDatabase dbWritable;
+    private static final String ENCRYPTION_KEY = MainActivity.activity.getResources().getString(R.string.ENCRYPTION_KEY);
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        SQLiteDatabase.loadLibs(context);
+        dbReadable = getReadableDatabase(ENCRYPTION_KEY);
+        dbWritable = getReadableDatabase(ENCRYPTION_KEY);
+        onCreate(getWritableDatabase(ENCRYPTION_KEY));
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String Assets = "CREATE TABLE IF NOT EXISTS ASSETS(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                "pid INTEGER DEFAULT 0,"+
+                "KEYWORD TEXT UNIQUE," +
+                "typeId INTEGER ," +
+                "FOREIGN KEY (pid) REFERENCES ASSETS(id) ON DELETE CASCADE ," +
+                "FOREIGN KEY (typeId) REFERENCES TYPES(id) ON DELETE CASCADE)";
+        sqLiteDatabase.execSQL(Assets);
+
+        String TYPES = "CREATE TABLE IF NOT EXISTS TYPES(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                "type TEXT," +
+                "icon_name TEXT)";
+        sqLiteDatabase.execSQL(TYPES);
+        String Notes = "CREATE TABLE IF NOT EXISTS NOTES(" +
+                "id INTEGER PRIMARY KEY," +
+                "title TEXT,"+
+                "content TEXT," +
+                "FOREIGN KEY (id) REFERENCES ASSETS(id) ON DELETE CASCADE)";
+        sqLiteDatabase.execSQL(Notes);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
+
+    public boolean insertIntoAssetsTable(String keyword, String typeId, int pid){
+        try {
+            dbWritable.beginTransaction();
+            String sqlQuery = "INSERT INTO ASSETS (KEYWORD, typeId,pid) VALUES (?,?,?)";
+            dbWritable.execSQL(sqlQuery, new Object[]{keyword, typeId,pid});
+            dbWritable.setTransactionSuccessful();
+        } catch(SQLiteConstraintException ee) {
+            MainActivity.activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MainActivity.activity, "Duplicate Keyword", Toast.LENGTH_SHORT).show();
+                }
+            });
+            return false;
+        }catch(Exception e) {
+            LogHandler.saveLog("Failed to insert into ASSETS table : " + e.getLocalizedMessage(),true);
+            return false;
+        } finally {
+            dbWritable.endTransaction();
+        }
+        return true;
+    }
+
+    public void insertIntoTypesTable(String id,String type,String icon_name){
+        try {
+            dbWritable.beginTransaction();
+            String sqlQuery = "INSERT INTO TYPES (id,type,icon_name) VALUES (?,?,?)";
+            dbWritable.execSQL(sqlQuery, new String[]{id,type,icon_name});
+            dbWritable.setTransactionSuccessful();
+        } catch (Exception e) {
+            try {
+                String sqlQuery2 = "UPDATE TYPES SET icon_name = ? WHERE type = ?";
+                dbWritable.execSQL(sqlQuery2, new String[]{icon_name, type});
+                dbWritable.setTransactionSuccessful();
+            }catch (Exception e1){
+                LogHandler.saveLog("Failed to insert into TYPES table : " + e1.getLocalizedMessage(),true);
+            }
+        } finally {
+            dbWritable.endTransaction();
+        }
+    }
+
+    public void insertIntoNotesTable(String id,String title,String content){
+        System.out.println("data for insert is : "+ id + " " + title + " "
+         + content);
+        try {
+            dbWritable.beginTransaction();
+
+            String sqlQuery = "INSERT OR REPLACE INTO NOTES (id, title, content) VALUES (?, ?, ?)";
+            dbWritable.execSQL(sqlQuery, new Object[]{id, title, content});
+
+            dbWritable.setTransactionSuccessful();
+        } catch (Exception e) {
+            LogHandler.saveLog("Failed to insert or update note: " + e.getLocalizedMessage(),true);
+        } finally {
+            dbWritable.endTransaction();
+        }
+        updateAssetsTable(id,title);
+    }
+
+    private void updateAssetsTable(String id, String title) {
+        try {
+            dbWritable.beginTransaction();
+            String sqlQuery = "UPDATE ASSETS SET KEYWORD = ? WHERE id = ?";
+            dbWritable.execSQL(sqlQuery, new Object[]{title, id});
+            dbWritable.setTransactionSuccessful();
+        } catch (Exception e) {
+            LogHandler.saveLog("Failed to update ASSETS table : " + e.getLocalizedMessage(),true);
+        } finally {
+            dbWritable.endTransaction();
+        }
+    }
+
+    public String getTypeId(String type){
+        Cursor cursor = null;
+        String typeId = "";
+        try {
+            String sqlQuery = "SELECT id FROM TYPES WHERE type=?";
+            cursor = dbReadable.rawQuery(sqlQuery, new String[]{type});
+            if (cursor != null && cursor.moveToFirst()) {
+                typeId = cursor.getString(0);
+                cursor.close();
+            }
+        } catch (Exception e) {
+            LogHandler.saveLog("Failed to get typeId from TYPES table : " + e.getLocalizedMessage(),true);
+        }finally {
+            return typeId;
+        }
+    }
+
+    public String[] getAsset(String id){
+        String sqlQuery = "SELECT * FROM ASSETS WHERE id = ?";
+        Cursor cursor = dbReadable.rawQuery(sqlQuery, new String[]{id});
+        String[] result;
+        if(cursor != null && cursor.moveToFirst()){
+            result = new String[]{cursor.getString(0),cursor.getString(1)
+                    ,cursor.getString(2),cursor.getString(3)};
+        }else{
+            result = new String[]{"","","",""};
+        }
+        cursor.close();
+        return result;
+    }
+
+    public int getParentId(int id){
+        String sqlQuery = "SELECT pid FROM ASSETS WHERE id=?";
+        Cursor cursor = dbReadable.rawQuery(sqlQuery, new String[]{String.valueOf(id)});
+        int parentId = 0;
+        if(cursor != null && cursor.moveToFirst()){
+            parentId = cursor.getInt(0);
+        }
+        cursor.close();
+        return parentId;
+    }
+
+    public void deleteAsset(int id){
+        dbWritable.beginTransaction();
+        String sqlQuery = "DELETE FROM ASSETS WHERE id = ?";
+        dbWritable.execSQL(sqlQuery, new Object[]{id});
+        dbWritable.setTransactionSuccessful();
+        dbWritable.endTransaction();
+    }
+    public ArrayList<String[]> getAssetIdByPid(int pid){
+        ArrayList<String[]> assets = new ArrayList<>();
+        try{
+            String sqlQuery = "SELECT id,keyword,typeId FROM ASSETS WHERE pid=?";
+            Cursor cursor = dbReadable.rawQuery(sqlQuery, new String[]{String.valueOf(pid)});
+            if(cursor != null && cursor.moveToFirst()){
+                do{
+                    String id = cursor.getString(0);
+                    String keyword = cursor.getString(1);
+                    String typeId = cursor.getString(2);
+                    String[] asset = {id,keyword,typeId,String.valueOf(pid)};
+                    assets.add(asset);
+                }while (cursor.moveToNext());
+            }
+            cursor.close();
+            return assets;
+        }catch (Exception e){
+            LogHandler.saveLog("Failed to get assets by pid : "+e.getLocalizedMessage(),true);
+        }
+        return assets;
+    }
+
+    public String[] getNote(int currentId) {
+        String[] note = new String[2];
+        String sqlQuery = "SELECT title,content FROM NOTES WHERE id=?";
+        Cursor cursor = dbReadable.rawQuery(sqlQuery, new String[]{String.valueOf(currentId)});
+        if (cursor != null && cursor.moveToFirst()) {
+            note[0] = cursor.getString(0);
+            note[1] = cursor.getString(1);
+        }
+        cursor.close();
+        return note;
+    }
+
+    public String getLastId() {
+        dbReadable.beginTransaction();
+        String sqlQuery = "SELECT last_insert_rowid()";
+        Cursor cursor = dbReadable.rawQuery(sqlQuery, null);
+        String lastId = "";
+        if (cursor != null && cursor.moveToFirst()) {
+            lastId = cursor.getString(0);
+        }
+        cursor.close();
+        dbReadable.endTransaction();
+        return lastId;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/growup/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.growup;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.widget.GridView;
+import android.content.pm.PackageManager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static Activity activity;
+    public static DBHelper dbHelper;
+    public static int currentId = 0;
+    public static SharedPreferences preferences;
+    public static GridAdapter adapter;
+    public static GridView gridView;
+    public static NoteHandler noteCreator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        activity = this;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        manageAccessThread.start();
+        try {
+            manageAccessThread.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        manageReadAndWritePermissonsThread.start();
+        try {
+            manageReadAndWritePermissonsThread.join();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        LogHandler.CreateLogFile();
+        LogHandler.saveLog("--------------------------new run----------------------------", false);
+        preferences = getPreferences(Context.MODE_PRIVATE);
+        dbHelper = new DBHelper(this);
+        Upgrade.versionHandler(preferences);
+
+        dbHelper.insertIntoTypesTable("2","folder","ic_folder");
+        dbHelper.insertIntoTypesTable("3","note","ic_note");
+
+        noteCreator = new NoteHandler();
+        gridView = findViewById(R.id.gridView);
+        adapter = new GridAdapter();
+        gridView.setAdapter(adapter);
+    }
+
+    Thread manageAccessThread = new Thread() {
+        @Override
+        public void run() {
+            try {
+                int buildSdkInt = Build.VERSION.SDK_INT;
+                if (buildSdkInt >= 30) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                        if (!Environment.isExternalStorageManager()) {
+                            Intent getPermission = new Intent();
+                            getPermission.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                            startActivity(getPermission);
+                            while (!Environment.isExternalStorageManager()){
+                                System.out.println("here " + Environment.isExternalStorageManager());
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    if (Environment.isExternalStorageManager()) {
+                        System.out.println("Starting to get access from your android device");
+                    }}
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    Thread manageReadAndWritePermissonsThread = new Thread() {
+        @Override
+        public void run() {
+            if (true){
+                int requestCode =1;
+                String[] permissions = {
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                };
+                boolean isWriteAndReadPermissionGranted = (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
+                        (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+                while(!isWriteAndReadPermissionGranted){
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                            (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED |
+                                    ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+                        ActivityCompat.requestPermissions(MainActivity.this, permissions, requestCode);
+                    }
+                    isWriteAndReadPermissionGranted = (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
+                            (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+                }
+            }
+        }
+    };
+}
